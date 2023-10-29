0634a56710785686b0a629801b955f00a2cb53a2
Alejandro Coello <102518051+a-coello@users.noreply.github.com>
2023-10-29 20:35:40 +0000

    commit

--- pipeline_examples/chain_responsibility.py
+++ pipeline_examples/chain_responsibility.py
@@ -6,13 +6,32 @@ from typing import Optional
 
 
 class Handler(ABC):
+    """
+    Abstract base class for a handler in a chain of responsibility.
+
+    A handler can be part of a chain of responsibility, where each handler in the chain has the opportunity to handle a
+    request. If a handler cannot handle the request, it passes the request on to the next handler in the chain.
+
+    Attributes:
+        next (Optional[Handler]): The next handler in the chain.
+
+    Methods:
+        set_next(handler: Handler) -> None: Set the next handler in the chain.
+        handle_click_event() -> None: Handle a click event by attempting to handle it with this handler. If this
+            handler cannot handle the event, it passes the event on to the next handler in the chain.
+        on_click() -> bool: Handle a click event. This method should be implemented by subclasses.
+    """
+
     def __init__(self):
         self.next: Optional[Handler] = None
 
-    def set_next(self, handler: Handler) -> None:
+    def set_next(self, handler: "Handler") -> None:
+        """Set the next handler in the chain."""
         self.next = handler
 
     def handle_click_event(self) -> None:
+        """Handle a click event by attempting to handle it with this handler. If this handler cannot handle the event,
+        it passes the event on to the next handler in the chain."""
         try:
             if self.on_click() and self.next:
                 self.next.handle_click_event()
@@ -21,7 +40,8 @@ class Handler(ABC):
 
     @abstractmethod
     def on_click(self) -> bool:
-        """Handle a click event."""
+        """Handle a click event. This method should be implemented by subclasses."""
+        pass
 
 
 @dataclass
